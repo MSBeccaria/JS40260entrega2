1b0b6c4811b990acc9557e848f26d5bfdd2e5c2a
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-08-29 15:34:22 -0300

    maqueta

--- index.js
+++ index.js
@@ -7,22 +7,3 @@ function calculadorImc(){
 
 calculadorImc()
 
-function resultado(){
-    if (resultado < 18.5){
-        alert("Tu peso estápor debajo del promedio saludable");
-    } else if (resultado > 18.5)
-        && (resultado > 24.9);{
-        alert("Tu peso es saludable");
-    }
-}
-
-
-/*switch(resultado){
-    case "<18.5":
-      return alert("Tu peso estápor debajo del promedio saludable" );
-      break;
-
-      case "<18,5 && >24,9":
-        return alert("Tu peso es saludable");
-         break;
-    
