28a5f90b6cedd11bb04760c3689c61bac3bed7ac
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-09-02 15:18:38 -0300

    js

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ let parametroResultado = calculadorImc()
 function resultado(resultadof1){
     if (resultadof1 < 18.5) { 
         alert("Tu peso está por debajo del promedio saludable");
-    } else if ((resultadof1 > 18.5)  && (resultadof1 < 24.9)); {
-        alert("Tu peso es saludable"); // hasta acá funciona, carga la página y salen los prompt y alert. Agregué más Else if con más parametros y dejó de funcionar
-    } else ((resultadof1 > 25)  && (resultadof1 < 29.9)); {
+    
+    } else if ((resultadof1 > 18.5) && (resultadof1 < 24.9)){
+        alert("Tu peso es saludable"); 
+    
+    } else if ((resultadof1 > 25)  && (resultadof1 < 29.9)) { 
         alert("Tienes sobrepeso");
-    } else if ((resultadof1 > 30)  && (resultadof1 < 99.9)); {
+
+    } else if ((resultadof1 > 30)  && (resultadof1 < 99.9)) {
         alert("Tu peso se considera obesidad. recomendamos consultar con un profesional de la salud");
-    } else if(resultadof1 > 40);{
-        alert("Recomendamos consultar con un profesional de la salud");
+    } 
+
+    else  { 
+        alert("Recomendamos consultar con un profesional de la salud")
     }
 }
 
-//a partir de la linea 19 deja de funcionar. Será que más de un ELSE IF no se puede poner?
+
 resultado(parametroResultado)
