c352715324c3e1b682d953afbedb578453b4237e
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-09-02 15:42:10 -0300

    js

--- index.js
+++ index.js
@@ -14,19 +14,14 @@ let parametroResultado = calculadorImc()
 function resultado(resultadof1){
     if (resultadof1 < 18.5) { 
         alert("Tu peso está por debajo del promedio saludable");
-    
-    } else if ((resultadof1 > 18.5) && (resultadof1 < 24.9)){
+     } else if ((resultadof1 > 18.5) && (resultadof1 < 24.9)) {
         alert("Tu peso es saludable"); 
-    
     } else if ((resultadof1 > 25)  && (resultadof1 < 29.9)) { 
         alert("Tienes sobrepeso");
-
-    } else if ((resultadof1 > 30)  && (resultadof1 < 99.9)) {
+    } else if ((resultadof1 > 30)  && (resultadof1 < 39.9)) {
         alert("Tu peso se considera obesidad. recomendamos consultar con un profesional de la salud");
-    } 
-
-    else  { 
-        alert("Recomendamos consultar con un profesional de la salud")
+    } else if(resultadof1 > 40) { 
+        alert("Recomendamos consultar con un profesional de la salud");
     }
 }
 
