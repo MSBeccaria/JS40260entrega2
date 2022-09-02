819883fd3fd9be9060fc2b9c1501c9c943b4881c
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-09-02 08:35:36 -0300

    js

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ function calculadorImc(){
     let peso = parseFloat (prompt("Ingresa tu peso en Kg")); 
     let altura = parseFloat (prompt("Ingresa tu altura en Mt")); 
     let resultado = alert(peso/(altura*altura)); 
-    return (resultado) //devolvemos solo el valor que necesitamos utilizar 
+    return (resultado) 
 }   
 
-//Recorda que los () indican que estas llamando a la funcion no importa si lo estas guardando o no en una variable
-
-let parametroResultado = calculadorImc() //guardamos en una variable el resultado de la funcion de arriba (como return me trae resultado se guarda ese valor en parametroResultado)
 
 
-// le indicamos a la funcion que le vamos a pasar un parametro 
-function resultado(resultadof1){  //para no confundirnos vamos a ponerle resultadoF1 (f1=funcion1)
+let parametroResultado = calculadorImc() 
+
+
+
+function resultado(resultadof1){
     if (resultadof1 < 18.5) { 
         alert("Tu peso está por debajo del promedio saludable");
     } else if ((resultadof1 > 18.5)  && (resultadof1 < 24.9)); {
-        alert("Tu peso es saludable");
-    } else if ((resultadof1 > 25)  && (resultadof1 < 29.9)); {
+        alert("Tu peso es saludable"); // hasta acá funciona, carga la página y salen los prompt y alert. Agregué más Else if con más parametros y dejó de funcionar
+    } else ((resultadof1 > 25)  && (resultadof1 < 29.9)); {
         alert("Tienes sobrepeso");
-} 
+    } else if ((resultadof1 > 30)  && (resultadof1 < 99.9)); {
+        alert("Tu peso se considera obesidad. recomendamos consultar con un profesional de la salud");
+    } else if(resultadof1 > 40);{
+        alert("Recomendamos consultar con un profesional de la salud");
+    }
+}
 
-//llamamos ala segunda funcion  y le pasamos el valor de la primera
+//a partir de la linea 19 deja de funcionar. Será que más de un ELSE IF no se puede poner?
 resultado(parametroResultado)
